2c4723d0b6c52347259cd13cbaa30ec917facd12
matteocenci <matteocenci01@gmail.com>
2024-05-08 15:39:24 +0200

    feat: api getting in console

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,26 @@
 <script>
+import axios from "axios";
+import {store} from "./store";
+export default {
+  data() {
+    return{
+      store,
+    };
+  },
+  created () {
+    axios.get(" https://api.themoviedb.org/3/search/tv?api_key=b2ead90c1236f96f4b0e9528732a8bfc", {
+      params: {
+        api_key: this.store.apiKey,
+        query: "Ciao"
+        
+        
+      },
+    })
+    .then((resp) => {
+      console.log(resp);
+    });
+  }
+}
 
 </script>
 
