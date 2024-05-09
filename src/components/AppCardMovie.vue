6a5bca777938143384dd3649f65ff0476779fe52
matteocenci <matteocenci01@gmail.com>
2024-05-09 10:22:06 +0200

    feat: AppCardMovie

---
+++ src/components/AppCardMovie.vue
@@ -0,0 +1,18 @@
+<script>
+
+    export default {
+        props: {
+            cardsobjMovie: Object
+        },
+
+        computed: {
+            title () {
+                return this.cardsobjMovie.original_title
+            }
+        }
+    }
+</script>
+
+<template>
+    <img :src="`https://image.tmdb.org/t/p/w342${cardsobjMovie.poster_path}`" alt="">
+</template>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,12 @@
 <script>
 import {store} from "../store";
 import AppCardTv from "./AppCardTv.vue";
+import AppCardMovie from "./AppCardMovie.vue";
 export default {
     components: {
+        AppCardMovie,
         AppCardTv,
+
     },
     data () {
         return {
@@ -20,10 +23,20 @@ export default {
 <template>
     <div class="container">
         <div class="row row-cols-4">
+            <div class="col" v-show="curMovie.poster_path" v-for="curMovie in store.moviesArray">
+                <AppCardMovie :cardsobjMovie="curMovie"/>
 
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="row row-cols-4">
+            
             <div class="col" v-show="curTv.poster_path" v-for="curTv in store.tvArray">
                 <AppCardTv :cardsObj="curTv"/>
             </div>
+
         </div>
     </div>
 
